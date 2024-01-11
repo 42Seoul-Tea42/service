4c6ef81582f4e71cd2cbc0588dd49e98ca980d72
JaeJunday <wowns8114@gmail.com>
2024-01-11 18:48:18 +0900

    fix: init script

--- frontend/init_react.sh
+++ frontend/init_react.sh
@@ -4,6 +4,6 @@ pnpm --prefix srcs install
 
 #pnpm add --prefix srcs --save-dev package-name
 
-# pnpm dev --prefix srcs
-pnpm --prefix srcs run build
-pnpm --prefix srcs start
+pnpm --prefix srcs run dev 
+# pnpm --prefix srcs run build
+# pnpm --prefix srcs start
