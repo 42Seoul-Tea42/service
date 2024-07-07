48bca105420503d1dc13404e63ef3c133b3a6caf
jaejun kim <wowns8114@gmail.com>
2024-07-07 14:31:16 +0900

    Delete nginx/out directory

--- nginx/out/_next/static/h-Sn0ul0RJCy7-9ATySJw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-cf2a1c4f07efd720.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
